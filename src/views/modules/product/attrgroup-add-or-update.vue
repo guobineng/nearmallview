a7a56b44a9ee9ef1fd63cd4f0b74fd7a589895dd
guobineng <2323498783@qq.com>
2021-06-25 15:45:33 +0800

    增加模块

--- src/views/modules/product/attrgroup-add-or-update.vue
+++ src/views/modules/product/attrgroup-add-or-update.vue
@@ -39,7 +39,7 @@
 </template>
 
 <script>
- import CategoryCascader from '../common/category-cascader'
+import CategoryCascader from '../common/category-cascader'
 export default {
   data() {
     return {
